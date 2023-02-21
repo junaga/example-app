1874c9c6d1c0169b7a96081dfd875dc6ef6b9e4c
junaga <hermann-stanew@invita.gmbh>
2023-02-21 22:54:19 +0000

    cleanup config files

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/**
+ * Next.js configuration file
+ * https://nextjs.org/docs/api-reference/next.config.js/introduction
+ * @type {import('next').NextConfig}
+ */
+export default {
+	reactStrictMode: true
+}
