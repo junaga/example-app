bb5fa067b89adf00b0692f8a0242ec145f6f5a46
junaga <hermann-stanew@invita.gmbh>
2023-02-22 03:07:54 +0000

    fix favicon

--- components/AppHead.tsx
+++ components/AppHead.tsx
@@ -17,13 +17,13 @@ export default function AppHead({ title }: { title: string }) {
 				rel="icon"
 				type="image/png"
 				sizes="32x32"
-				href="/favicon/favicon-32x32.png"
+				href="/favicon/32x32.png"
 			/>
 			<link
 				rel="icon"
 				type="image/png"
 				sizes="16x16"
-				href="/favicon/favicon-16x16.png"
+				href="/favicon/16x16.png"
 			/>
 			<link rel="manifest" href="/site.webmanifest" />
 		</Head>
