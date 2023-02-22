a966a6cc0d7e26f6a096c56718f799d89a77f9b2
junaga <hermann-stanew@invita.gmbh>
2023-02-22 02:04:36 +0000

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
 import "~/styles/globals.css"
 import type { AppProps } from "next/app"
+import Head from "next/head"
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/favicon/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
