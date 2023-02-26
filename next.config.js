0a134a675e20f1b95112e50d393ca13bade23ee0
junaga <hermann-stanew@invita.gmbh>
2023-02-26 10:47:09 +0000

    del generated skeleton examples

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+
+/** @type {import('next').NextConfig} */
+module.exports = {
+	reactStrictMode: true,
+	i18n: {
+		locales: ["de-DE"],
+		defaultLocale: "de-DE"
+	}
+}
