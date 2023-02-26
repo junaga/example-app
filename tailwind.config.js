e3ef25632c3dea68c68f108611d54d8129282868
junaga <hermann-stanew@invita.gmbh>
2023-02-26 10:41:31 +0000

    configure daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
+// https://tailwindcss.com/docs/configuration
+const typography = require("@tailwindcss/typography")
 const daisyui = require("daisyui")
+const themes = require("daisyui/src/colors/themes")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./pages/**/*.tsx", "./lib/**/*.tsx"],
-	theme: {
-		extend: {}
-	},
-	plugins: [daisyui]
+	plugins: [typography, daisyui],
+
+	// https://daisyui.com/docs/config/
+	daisyui: {
+		themes: [
+			{
+				myLemonade: {
+					...themes["[data-theme=lemonade]"],
+					primary: "#8C0327" // autumn red #8C0327 // generate button red #FC0404
+				}
+			}
+		]
+	}
 }
