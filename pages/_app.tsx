2187977a80e49c1ceff6f483f45c5614549fa4a4
junaga <hermann-stanew@invita.gmbh>
2023-02-27 15:45:14 +0000

    add "Labrada" font

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from "next/app"
+import Head from "next/head"
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<>
+			<Head>
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css2?family=Labrada&display=swap"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
+}
