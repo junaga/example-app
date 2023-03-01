f154d02ded4b6164b9954f8ff8de5863f93f1ca5
junaga <hermann-stanew@invita.gmbh>
2023-03-01 11:08:20 +0000

    add `/openai` endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,39 @@
 import AppHead from "~/lib/AppHead"
 import Header from "~/lib/Header"
+const endpoint = "/api/openai"
+import type { RequestBody, ResponseBody } from "./api/openai"
 
 const title = "Online Bewerbung mit KI generieren lassen - Bewerbung.gg"
+
+async function submit(event: any) {
+	event.preventDefault()
+
+	const body: RequestBody = { prompt: event.target.prompt.value }
+	const response = await fetch(endpoint, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(body)
+	})
+	const data: ResponseBody = await response.json()
+
+	console.log(data.choices)
+}
 
 export default function IndexPage() {
 	return (
 		<>
 			<AppHead title={title} />
-			<Header />
 			<main>
-				{Array.from({ length: 1000 }).map((_, i) => (
-					<p key={i}>Line {i}</p>
-				))}
+				<form onSubmit={submit}>
+					<input type="text" name="prompt" />
+					<input type="submit" value="Generate names" />
+				</form>
 			</main>
 
 			<style jsx>
 				{`
 					main {
-						display: flex;
-						flex-direction: column;
-						align-items: center;
+						height: 100vh;
 					}
 				`}
 			</style>
