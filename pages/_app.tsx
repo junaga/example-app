90483af567d1c31ba132b48f3b5259f94ed25bc7
junaga <hermann-stanew@invita.gmbh>
2023-03-02 20:08:29 +0000

    fmt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,7 @@ import type { AppProps } from "next/app"
 import Head from "next/head"
 import "~/lib/globals.css"
 
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
-	// use google font to add "Labrada" font
-	//
 	return (
 		<>
 			<Head>
