340b393940e697d453427a419d4f59f3366423dd
junaga <hermann-stanew@invita.gmbh>
2023-03-03 05:39:02 +0000

    add print.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app"
 import Head from "next/head"
 import "~/lib/index.css"
+import "~/lib/print.css"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
