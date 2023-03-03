d63d075e20dde1a23a163210b20b468dc7574982
junaga <hermann-stanew@invita.gmbh>
2023-03-03 04:10:40 +0000

    rename `index.css`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app"
 import Head from "next/head"
-import "~/lib/globals.css"
+import "~/lib/index.css"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
